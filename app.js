fc3a6c55e1efc49470a52cba8905bab2eea5863a
Anastasia Nistratova <a.nistratova14@gmail.com>
2023-07-04 11:29:23 +0300

    feat: add helmet and Express Rate Limit

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 // для консольлогов использовать 'eslint-disable-next-line no-console'
 const express = require('express');
 const mongoose = require('mongoose');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
 const app = express();
+
+// параметры Express Rate Limit
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+// защита приложения от некоторых широко известных веб-уязвимостей
+app.use(helmet());
+// ограничение кол-во запросов. Для защиты от DoS-атак.
+app.use(limiter);
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
