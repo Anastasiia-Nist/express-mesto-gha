c5c310797cb61d0cf7256d6fa85eaa8a71af4061
Anastasia Nistratova <a.nistratova14@gmail.com>
2023-07-21 10:27:12 +0300

    fix: 14ПР add .env variables

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
 
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
 // параметры Express Rate Limit
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -30,7 +32,7 @@ app.use(helmet());
 app.use(limiter);
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect(DB_URL);
 
 app.use(bodyParser.json());
 // app.use(cookieParser());
@@ -51,4 +53,4 @@ app.use((req, res, next) => {
 app.use(errors()); // обработчик ошибок celebrate
 app.use(errorsMiddleware); // централизованный обработчик
 
-app.listen(3000);
+app.listen(PORT);
